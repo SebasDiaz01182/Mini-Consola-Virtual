a0916f03ce10076e59663e641adad1ec611dc855
Luis Araya Mata <luistec2309@estudiantec.cr>
2020-11-25 11:43:26 -0600

    Clases iniciales

---
+++ MiniConsolaVirtual/src/main/java/com/mycompany/miniconsolavirtual/Controlador.java
@@ -0,0 +1,37 @@
+
+package com.mycompany.miniconsolavirtual;
+
+
+public class Controlador extends javax.swing.JFrame {
+
+   
+    public Controlador() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 876, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 738, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+   
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
---
+++ MiniConsolaVirtual/src/main/java/com/mycompany/miniconsolavirtual/Modelo.java
@@ -0,0 +1,6 @@
+
+package com.mycompany.miniconsolavirtual;
+
+public class Modelo {
+    
+}
