98d733cde98aecf418d002355f94e2dd725ba750
Sebastián Díaz Obando <sebascout01182@gmail.com>
2020-11-26 12:48:06 -0600

    Mini adelanto de la parte grafica

--- MiniConsolaVirtual/src/main/java/com/mycompany/miniconsolavirtual/Controlador.java
+++ MiniConsolaVirtual/src/main/java/com/mycompany/miniconsolavirtual/Controlador.java
@@ -14,18 +14,25 @@ public class Controlador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        BotonSuperior = new javax.swing.JLabel();
+        BotonDerecho = new javax.swing.JLabel();
+        BotonIzquierdo = new javax.swing.JLabel();
+        BotonInferior = new javax.swing.JLabel();
+        BotonA = new javax.swing.JLabel();
+        BotonB = new javax.swing.JLabel();
+        Fondo = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 876, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 738, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(BotonSuperior, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 250, 40, 40));
+        getContentPane().add(BotonDerecho, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 290, 40, 40));
+        getContentPane().add(BotonIzquierdo, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 290, 50, 40));
+        getContentPane().add(BotonInferior, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 330, 40, 40));
+        getContentPane().add(BotonA, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 300, 70, 70));
+        getContentPane().add(BotonB, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 300, 70, 70));
+
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Controler.jpg"))); // NOI18N
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 990, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -33,5 +40,12 @@ public class Controlador extends javax.swing.JFrame {
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BotonA;
+    private javax.swing.JLabel BotonB;
+    private javax.swing.JLabel BotonDerecho;
+    private javax.swing.JLabel BotonInferior;
+    private javax.swing.JLabel BotonIzquierdo;
+    private javax.swing.JLabel BotonSuperior;
+    private javax.swing.JLabel Fondo;
     // End of variables declaration//GEN-END:variables
 }
